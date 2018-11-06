0b2e403a36ff3946d34a37aebb069dc80e1f96db
tatyana12 <tatyana_pol@yahoo.com>
2018-11-05 17:17:50 -0800

    M

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files from the dist directory
-app.use(express.static(__dirname + '/dist/conference-app'));
+app.use(express.static(__dirname + '/dist/conference-last'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/conference-app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/conference-last/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
